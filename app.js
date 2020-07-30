b2508592dcf967a218cbc55e62fbece30a1e4f19
Nick Naumenko <carswithcharacter614@gmail.com>
2020-07-30 17:28:05 -0400

    formatted minutes, seconds and milliseconds to have two digits

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let timer = 0;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
 let timerSwitch = "off";
 const screen = document.querySelector(".display");
 const startButton = document.querySelector(".start");
@@ -8,7 +11,18 @@ const resetButton = document.querySelector(".reset");
 function startTimer() {
   setInterval(() => {
     if (timerSwitch === "on") timer++;
-    screen.innerText = timer;
+    minutes = Math.floor(timer / 6000);
+    seconds = Math.floor((timer % 6000) / 100);
+    milliseconds = Math.floor(timer % 100);
+    screen.innerText =
+      (minutes < 10 ? "0" : "") +
+      minutes +
+      ":" +
+      (seconds < 10 ? "0" : "") +
+      seconds +
+      "." +
+      (milliseconds < 10 ? "0" : "") +
+      milliseconds;
   }, 10);
 }
 
@@ -20,9 +34,9 @@ pauseButton.addEventListener("click", () => {
   timerSwitch = "off";
 });
 
-resetButton.addEventListener ("click", () => {
-    timerSwitch = "off"
-    timer = 0;
-})
+resetButton.addEventListener("click", () => {
+  timerSwitch = "off";
+  timer = 0;
+});
 
 startTimer();
